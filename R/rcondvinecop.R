522c47a20f2e258b27bb519075971f293e821355
EmanuelSommer <emanuel.sommer@web.de>
2021-11-22 17:03:23 +0100

    optimize cond sampling with internal functions but caution CRAN does not allow this

--- R/rcondvinecop.R
+++ R/rcondvinecop.R
@@ -23,10 +23,10 @@ r1conddvine <- function(n_samples, cond_alpha, fitted_vine){
   n_assets <- length(asset_names)
   # for each cond_alpha level get the n_samples samples
   sample_dt <- lapply(cond_alpha, function(cond_alpha_level) {
-    sample_matrix <- replicate(n_samples, {
+    replicate(n_samples, {
       aux_matrix <- matrix(data = NA_real_,
-                              nrow = n_assets,
-                              ncol = n_assets)
+                           nrow = n_assets,
+                           ncol = n_assets)
       # initialize the diagonal with the conditional value as well as the
       # sampled values
       diag(aux_matrix) <- c(cond_alpha_level, stats::runif(n_assets - 1))
@@ -34,28 +34,20 @@ r1conddvine <- function(n_samples, cond_alpha, fitted_vine){
       for (j in 2:n_assets) {
         for (k in (j - 1):1) {
           current_bicop <- fitted_vine$pair_copulas[[k]][[j - k]]
-          aux_matrix[k, j] <- rvinecopulib::hbicop(
+          aux_matrix[k, j] <- rvinecopulib:::bicop_hinv2_cpp(
             u = matrix(
               c(aux_matrix[k + 1, j], aux_matrix[k, j - 1]),
               ncol = 2
             ),
-            cond_var = 2,
-            family = current_bicop$family,
-            rotation = current_bicop$rotation,
-            parameters = current_bicop$parameters,
-            inverse = TRUE
+            bicop_r = current_bicop
           )
           if (j < n_assets) {
-            aux_matrix[k + 1, j] <- rvinecopulib::hbicop(
+            aux_matrix[k + 1, j] <- rvinecopulib:::bicop_hfunc2_cpp(
               u = matrix(
                 c(aux_matrix[k, j - 1], aux_matrix[k, j]),
                 ncol = 2
               ),
-              cond_var = 2,
-              family = current_bicop$family,
-              rotation = current_bicop$rotation,
-              parameters = current_bicop$parameters,
-              inverse = FALSE
+              bicop_r = current_bicop
             )
           }
         }
@@ -64,18 +56,21 @@ r1conddvine <- function(n_samples, cond_alpha, fitted_vine){
       # the conditioning value
       aux_matrix[1, ]
     }) %>%
-      t()
-
-    reorder_indices <- sapply(seq(asset_names), function(ind) {
-      which(fitted_vine$structure$order == ind)}, simplify = TRUE)
-    sample_matrix <- sample_matrix[, reorder_indices]
-    colnames(sample_matrix) <- asset_names
-    data.table::as.data.table(sample_matrix)
+      t() %>%
+      data.table::as.data.table()
   }) %>% data.table::rbindlist()
+
+  # reorder the columns in the original order
+  reorder_indices <- sapply(seq(asset_names), function(ind) {
+    which(fitted_vine$structure$order == ind)}, simplify = TRUE)
+  data.table::setcolorder(sample_dt, reorder_indices)
+  colnames(sample_dt) <- asset_names
 
   list(sample_dt = sample_dt,
        cond_alpha_vec = sample_dt[[fitted_vine$structure$order[1]]])
 }
+
+
 
 #' Sample conditioned on the two rightmost edge variables from a D-vine copula
 #'
@@ -102,20 +97,16 @@ r2conddvine <- function(n_samples, cond_alpha, fitted_vine) {
   # conditional value. To mimic the cond_alpha level quantile of both
   # conditional variables the conditional value of the rightmost variable will
   # be computed based on the copula between the two variables
-  cond_alpha_second <- rvinecopulib::hbicop(
+  cond_alpha_second <- rvinecopulib:::bicop_hinv2_cpp(
     u = matrix(
       c(cond_alpha, cond_alpha),
       ncol = 2, byrow = FALSE,
     ),
-    cond_var = 2,
-    family = fitted_vine$pair_copulas[[1]][[1]]$family,
-    rotation = fitted_vine$pair_copulas[[1]][[1]]$rotation,
-    parameters = fitted_vine$pair_copulas[[1]][[1]]$parameters,
-    inverse = TRUE
+    bicop_r = fitted_vine$pair_copulas[[1]][[1]]
   )
   # for each cond_alpha level get the n_samples samples
   sample_dt <- lapply(seq(length(cond_alpha)), function(cond_alpha_ind) {
-    sample_matrix <- replicate(n_samples, {
+    replicate(n_samples, {
       aux_matrix <- matrix(data = NA_real_,
                            nrow = n_assets,
                            ncol = n_assets)
@@ -127,16 +118,12 @@ r2conddvine <- function(n_samples, cond_alpha, fitted_vine) {
 
       diag(aux_matrix) <- c(
         cond_alpha[cond_alpha_ind],
-        rvinecopulib::hbicop(
+        rvinecopulib:::bicop_hfunc2_cpp(
           u = matrix(
             c(cond_alpha[cond_alpha_ind], cond_alpha_second[cond_alpha_ind]),
             ncol = 2, byrow = FALSE,
           ),
-          cond_var = 2,
-          family = fitted_vine$pair_copulas[[1]][[1]]$family,
-          rotation = fitted_vine$pair_copulas[[1]][[1]]$rotation,
-          parameters = fitted_vine$pair_copulas[[1]][[1]]$parameters,
-          inverse = FALSE
+          bicop_r = fitted_vine$pair_copulas[[1]][[1]]
         ),
         stats::runif(n_assets - 2)
       )
@@ -144,28 +131,20 @@ r2conddvine <- function(n_samples, cond_alpha, fitted_vine) {
       for (j in 3:n_assets) {
         for (k in (j - 1):1) {
           current_bicop <- fitted_vine$pair_copulas[[k]][[j - k]]
-          aux_matrix[k, j] <- rvinecopulib::hbicop(
+          aux_matrix[k, j] <- rvinecopulib:::bicop_hinv2_cpp(
             u = matrix(
               c(aux_matrix[k + 1, j], aux_matrix[k, j - 1]),
               ncol = 2
             ),
-            cond_var = 2,
-            family = current_bicop$family,
-            rotation = current_bicop$rotation,
-            parameters = current_bicop$parameters,
-            inverse = TRUE
+            bicop_r = current_bicop
           )
           if (j < n_assets) {
-            aux_matrix[k + 1, j] <- rvinecopulib::hbicop(
+            aux_matrix[k + 1, j] <- rvinecopulib:::bicop_hfunc2_cpp(
               u = matrix(
                 c(aux_matrix[k, j - 1], aux_matrix[k, j]),
                 ncol = 2
               ),
-              cond_var = 2,
-              family = current_bicop$family,
-              rotation = current_bicop$rotation,
-              parameters = current_bicop$parameters,
-              inverse = FALSE
+              bicop_r = current_bicop
             )
           }
         }
@@ -174,13 +153,15 @@ r2conddvine <- function(n_samples, cond_alpha, fitted_vine) {
       # the conditioning value
       aux_matrix[1, ]
     }) %>%
-      t()
-    reorder_indices <- sapply(seq(asset_names), function(ind) {
-      which(fitted_vine$structure$order == ind)}, simplify = TRUE)
-    sample_matrix <- sample_matrix[, reorder_indices]
-    colnames(sample_matrix) <- asset_names
-    data.table::as.data.table(sample_matrix)
+      t() %>%
+      data.table::as.data.table()
   }) %>% data.table::rbindlist()
+
+  # reorder the columns in the original order
+  reorder_indices <- sapply(seq(asset_names), function(ind) {
+    which(fitted_vine$structure$order == ind)}, simplify = TRUE)
+  data.table::setcolorder(sample_dt, reorder_indices)
+  colnames(sample_dt) <- asset_names
 
   list(sample_dt = sample_dt,
        cond_alpha_vec = sample_dt[[fitted_vine$structure$order[1]]])
