22530e78cc93de08347320c0d89f765effb23a32
EmanuelSommer <emanuel.sommer@web.de>
2021-12-21 13:12:01 +0100

    some fixes in the c++ code

--- src/dvinecond.cpp
+++ src/dvinecond.cpp
@@ -13,10 +13,6 @@ Eigen::MatrixXd
     auto vine_struct_ = vinecop_cpp.get_rvine_structure();
     auto copula_dimension = vine_struct_.get_dim();
 
-    // type vector<long long unsigned int>
-    // auto order = vine_struct_.get_order();
-    // auto inverse_order = tools_stl::invert_permutation(order);
-
     // initialize output matrix with n_samples rows and copula dimension columns
     Eigen::MatrixXd output_matrix(n_samples, int(copula_dimension));
 
@@ -35,16 +31,17 @@ Eigen::MatrixXd
 
       // iterate over the auxiliary matrix (columns first then rows)
       // according to the conditional sampling algorithm
+      // first initialize two additional matrices to save hfunction inputs
+      Eigen::MatrixXd hinv_input(1, 2);
+      Eigen::MatrixXd hfunc_input(1, 2);
       for (size_t column_iter = 1; column_iter < copula_dimension; ++column_iter) {
         for (size_t row_iter = column_iter - 1; row_iter < copula_dimension; --row_iter) {
           tools_interface::check_user_interrupt();
           auto edge_copula = vinecop_cpp.get_pair_copula(row_iter, column_iter - 1 - row_iter);
-          Eigen::MatrixXd hinv_input(1, 2);
           hinv_input(0, 0) = auxiliary_matrix(row_iter + 1, column_iter);
           hinv_input(0, 1) = auxiliary_matrix(row_iter, column_iter - 1);
           auxiliary_matrix(row_iter, column_iter) = edge_copula.hinv2(hinv_input)(0, 0);
-          if (column_iter < copula_dimension) {
-            Eigen::MatrixXd hfunc_input(1, 2);
+          if (column_iter < copula_dimension - 1) {
             hfunc_input(0, 0) = auxiliary_matrix(row_iter, column_iter - 1);
             hfunc_input(0, 1) = auxiliary_matrix(row_iter, column_iter);
             auxiliary_matrix(row_iter + 1, column_iter) = edge_copula.hfunc2(hfunc_input)(0, 0);
@@ -99,16 +96,17 @@ Eigen::MatrixXd
 
       // iterate over the auxiliary matrix (columns first then rows)
       // according to the conditional sampling algorithm
+      // first initialize two additional matrices to save hfunction inputs
+      Eigen::MatrixXd hinv_input(1, 2);
+      Eigen::MatrixXd hfunc_input(1, 2);
       for (size_t column_iter = 2; column_iter < copula_dimension; ++column_iter) {
         for (size_t row_iter = column_iter - 1; row_iter < copula_dimension; --row_iter) {
           tools_interface::check_user_interrupt();
           auto edge_copula = vinecop_cpp.get_pair_copula(row_iter, column_iter - 1 - row_iter);
-          Eigen::MatrixXd hinv_input(1, 2);
           hinv_input(0, 0) = auxiliary_matrix(row_iter + 1, column_iter);
           hinv_input(0, 1) = auxiliary_matrix(row_iter, column_iter - 1);
           auxiliary_matrix(row_iter, column_iter) = edge_copula.hinv2(hinv_input)(0, 0);
-          if (column_iter < copula_dimension) {
-            Eigen::MatrixXd hfunc_input(1, 2);
+          if (column_iter < copula_dimension - 1) {
             hfunc_input(0, 0) = auxiliary_matrix(row_iter, column_iter - 1);
             hfunc_input(0, 1) = auxiliary_matrix(row_iter, column_iter);
             auxiliary_matrix(row_iter + 1, column_iter) = edge_copula.hfunc2(hfunc_input)(0, 0);
