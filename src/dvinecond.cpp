abadde78d2680144a272750a6e2cab5b5969822b
EmanuelSommer <emanuel.sommer@web.de>
2021-12-21 20:14:53 +0100

    clean up c++ code

--- src/dvinecond.cpp
+++ src/dvinecond.cpp
@@ -2,6 +2,29 @@
 
 using namespace vinecopulib;
 
+void iterate_aux_matrix(int start_column,
+                                   int copula_dimension,
+                                   Vinecop vinecop,
+                                   Eigen::MatrixXd& auxiliary) {
+  Eigen::MatrixXd hinv_input(1, 2);
+  Eigen::MatrixXd hfunc_input(1, 2);
+  for (size_t column = start_column; column < copula_dimension; ++column) {
+    for (int row = column - 1; row >= 0; --row) {
+      tools_interface::check_user_interrupt();
+      auto edge_copula = vinecop.get_pair_copula(row, column - 1 - row);
+      hinv_input(0, 0) = auxiliary(row + 1, column);
+      hinv_input(0, 1) = auxiliary(row, column - 1);
+      auxiliary(row, column) = edge_copula.hinv2(hinv_input)(0, 0);
+      if (column < copula_dimension - 1) {
+        hfunc_input(0, 0) = auxiliary(row, column - 1);
+        hfunc_input(0, 1) = auxiliary(row, column);
+        auxiliary(row + 1, column) = edge_copula.hfunc2(hfunc_input)(0, 0);
+      }
+    }
+  }
+}
+
+
 // [[Rcpp::export]]
 Eigen::MatrixXd
   cond_dvine1_cpp(
@@ -17,39 +40,25 @@ Eigen::MatrixXd
     Eigen::MatrixXd output_matrix(n_samples, int(copula_dimension));
 
     // create auxiliary matrix to build up the sample
-    Eigen::MatrixXd auxiliary_matrix(copula_dimension, copula_dimension);
+    Eigen::MatrixXd auxiliary(copula_dimension, copula_dimension);
 
     // first loop over n_samples (just repeat n_samples times)
     for (size_t sample = 0; sample < n_samples; ++sample) {
       // create the uniform samples
       Rcpp::NumericVector uniform_samples = Rcpp::runif(copula_dimension - 1);
       // set the diagonal
-      auxiliary_matrix(0, 0) = cond_alpha;
-      for(int i = 1; i < auxiliary_matrix.rows(); ++i) {
-        auxiliary_matrix(i, i) = uniform_samples[i - 1];
+      auxiliary(0, 0) = cond_alpha;
+      for(int i = 1; i < auxiliary.rows(); ++i) {
+        auxiliary(i, i) = uniform_samples[i - 1];
       }
 
       // iterate over the auxiliary matrix (columns first then rows)
       // according to the conditional sampling algorithm
       // first initialize two additional matrices to save hfunction inputs
-      Eigen::MatrixXd hinv_input(1, 2);
-      Eigen::MatrixXd hfunc_input(1, 2);
-      for (size_t column_iter = 1; column_iter < copula_dimension; ++column_iter) {
-        for (size_t row_iter = column_iter - 1; row_iter < copula_dimension; --row_iter) {
-          tools_interface::check_user_interrupt();
-          auto edge_copula = vinecop_cpp.get_pair_copula(row_iter, column_iter - 1 - row_iter);
-          hinv_input(0, 0) = auxiliary_matrix(row_iter + 1, column_iter);
-          hinv_input(0, 1) = auxiliary_matrix(row_iter, column_iter - 1);
-          auxiliary_matrix(row_iter, column_iter) = edge_copula.hinv2(hinv_input)(0, 0);
-          if (column_iter < copula_dimension - 1) {
-            hfunc_input(0, 0) = auxiliary_matrix(row_iter, column_iter - 1);
-            hfunc_input(0, 1) = auxiliary_matrix(row_iter, column_iter);
-            auxiliary_matrix(row_iter + 1, column_iter) = edge_copula.hfunc2(hfunc_input)(0, 0);
-          }
-        }
-      }
+      iterate_aux_matrix(1, copula_dimension,
+                         vinecop_cpp, auxiliary);
       // write samples to row of the the output matrix
-      output_matrix.row(sample) = auxiliary_matrix.row(0);
+      output_matrix.row(sample) = auxiliary.row(0);
     }
     return output_matrix;
   }
@@ -70,7 +79,7 @@ Eigen::MatrixXd
     Eigen::MatrixXd output_matrix(n_samples, int(copula_dimension));
 
     // create auxiliary matrix to build up the sample
-    Eigen::MatrixXd auxiliary_matrix(copula_dimension, copula_dimension);
+    Eigen::MatrixXd auxiliary(copula_dimension, copula_dimension);
 
     // calculate the second conditional value and a needed h function value
     Eigen::MatrixXd second_cond_input(1, 2);
@@ -86,35 +95,22 @@ Eigen::MatrixXd
       // create the uniform samples
       Rcpp::NumericVector uniform_samples = Rcpp::runif(copula_dimension - 2);
       // set the fixed conditioning values
-      auxiliary_matrix(0, 0) = second_cond_alpha;
-      auxiliary_matrix(0, 1) = cond_alpha;
-      auxiliary_matrix(1, 1) = cond_hfunc_value;
+      auxiliary(0, 0) = second_cond_alpha;
+      auxiliary(0, 1) = cond_alpha;
+      auxiliary(1, 1) = cond_hfunc_value;
       // set the random samples on the diagonal
-      for(int i = 2; i < auxiliary_matrix.rows(); ++i) {
-        auxiliary_matrix(i, i) = uniform_samples[i - 2];
+      for(int i = 2; i < auxiliary.rows(); ++i) {
+        auxiliary(i, i) = uniform_samples[i - 2];
       }
 
       // iterate over the auxiliary matrix (columns first then rows)
       // according to the conditional sampling algorithm
       // first initialize two additional matrices to save hfunction inputs
-      Eigen::MatrixXd hinv_input(1, 2);
-      Eigen::MatrixXd hfunc_input(1, 2);
-      for (size_t column_iter = 2; column_iter < copula_dimension; ++column_iter) {
-        for (size_t row_iter = column_iter - 1; row_iter < copula_dimension; --row_iter) {
-          tools_interface::check_user_interrupt();
-          auto edge_copula = vinecop_cpp.get_pair_copula(row_iter, column_iter - 1 - row_iter);
-          hinv_input(0, 0) = auxiliary_matrix(row_iter + 1, column_iter);
-          hinv_input(0, 1) = auxiliary_matrix(row_iter, column_iter - 1);
-          auxiliary_matrix(row_iter, column_iter) = edge_copula.hinv2(hinv_input)(0, 0);
-          if (column_iter < copula_dimension - 1) {
-            hfunc_input(0, 0) = auxiliary_matrix(row_iter, column_iter - 1);
-            hfunc_input(0, 1) = auxiliary_matrix(row_iter, column_iter);
-            auxiliary_matrix(row_iter + 1, column_iter) = edge_copula.hfunc2(hfunc_input)(0, 0);
-          }
-        }
-      }
+      iterate_aux_matrix(2, copula_dimension,
+                         vinecop_cpp, auxiliary);
+
       // write samples to row of the the output matrix
-      output_matrix.row(sample) = auxiliary_matrix.row(0);
+      output_matrix.row(sample) = auxiliary.row(0);
     }
     return output_matrix;
   }
